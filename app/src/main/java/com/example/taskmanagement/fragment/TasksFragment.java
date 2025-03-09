9c040d988a25b5dbedc11c319b7d7d96a9378fe9
sopheak <hangsopheak.hod@gmail.com>
2025-03-09 15:15:59 +0700

    Add recycler view

--- app/src/main/java/com/example/taskmanagement/fragment/TasksFragment.java
+++ app/src/main/java/com/example/taskmanagement/fragment/TasksFragment.java
@@ -3,16 +3,24 @@ package com.example.taskmanagement.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.taskmanagement.R;
+import com.example.taskmanagement.adapter.TaskAdapter;
+import com.example.taskmanagement.databinding.FragmentTasksBinding;
+import com.example.taskmanagement.service.TaskData;
+import com.google.android.material.carousel.CarouselLayoutManager;
 
 
 public class TasksFragment extends Fragment {
 
+
+    FragmentTasksBinding binding;
+    private TaskAdapter taskAdapter;
     public TasksFragment() {
         // Required empty public constructor
     }
@@ -20,7 +28,17 @@ public class TasksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
+        binding = FragmentTasksBinding.inflate(inflater, container, false);
+
+        binding.rcvTasks.setLayoutManager(new LinearLayoutManager(getContext()));
+        taskAdapter = new TaskAdapter(TaskData.SAMPLE_TASKS);
+        binding.rcvTasks.setAdapter(taskAdapter);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
