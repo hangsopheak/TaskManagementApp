5108207c9c400a25fa7f18dd76b9188b55f48bfe
sopheak <hangsopheak.hod@gmail.com>
2025-03-09 12:35:53 +0700

    Add fragments

--- app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.taskmanagement.databinding.ActivityMainBinding;
+import com.example.taskmanagement.fragment.CalendarFragment;
+import com.example.taskmanagement.fragment.CategoriesFragment;
+import com.example.taskmanagement.fragment.SettingFragment;
+import com.example.taskmanagement.fragment.TasksFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +27,31 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+           int itemId = item.getItemId();
+           if (itemId == R.id.nav_tasks) {
+               LoadFragment(new TasksFragment());
+           }else if(itemId == R.id.nav_categories){
+               LoadFragment(new CategoriesFragment());
+           }else if(itemId == R.id.nav_calendar){
+               LoadFragment(new CalendarFragment());
+           }
+           else if(itemId == R.id.nav_setting){
+               LoadFragment(new SettingFragment());
+           }
+           else{
+               return false;
+           }
+            return true;
+        });
 
+        binding.bottomNavigation.setSelectedItemId(R.id.nav_tasks);
 
+    }
+
+    private void LoadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
