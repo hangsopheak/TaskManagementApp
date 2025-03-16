a7da07d6ddc0878f5895f0f593b037c556ed1c9e
sopheak <hangsopheak.hod@gmail.com>
2025-03-16 11:36:53 +0700

    Week 5 demo m3

---
+++ app/src/main/java/com/example/taskmanagement/fragment/TasksFragment.java
@@ -0,0 +1,39 @@
+package com.example.taskmanagement.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.taskmanagement.R;
+import com.example.taskmanagement.adapter.TaskListAdapter;
+import com.example.taskmanagement.databinding.FragmentTasksBinding;
+import com.example.taskmanagement.service.TaskData;
+
+public class TasksFragment extends Fragment {
+
+    FragmentTasksBinding binding;
+    TaskListAdapter taskAdapter;
+
+    public TasksFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        binding = FragmentTasksBinding.inflate(inflater, container, false);
+
+        binding.rcTaskList.setLayoutManager(new LinearLayoutManager(getContext()));
+        taskAdapter = new TaskListAdapter(TaskData.SAMPLE_TASKS);
+        binding.rcTaskList.setAdapter(taskAdapter);
+        return binding.getRoot();
+
+    }
+}
