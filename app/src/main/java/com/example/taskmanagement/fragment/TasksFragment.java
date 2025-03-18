27d037dd7e25ff41a626c9b916cd558f17a4db1c
sopheak <hangsopheak.hod@gmail.com>
2025-03-18 19:37:01 +0700

    week 6 - task list

--- app/src/main/java/com/example/taskmanagement/fragment/TasksFragment.java
+++ app/src/main/java/com/example/taskmanagement/fragment/TasksFragment.java
@@ -2,24 +2,37 @@ package com.example.taskmanagement.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.taskmanagement.R;
 import com.example.taskmanagement.adapter.TaskAdapter;
 import com.example.taskmanagement.databinding.FragmentTasksBinding;
-import com.example.taskmanagement.service.TaskData;
+import com.example.taskmanagement.model.Task;
+import com.example.taskmanagement.repository.IApiCallback;
+import com.example.taskmanagement.repository.TaskRepository;
 import com.google.android.material.carousel.CarouselLayoutManager;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
 
 
 public class TasksFragment extends Fragment {
 
 
     FragmentTasksBinding binding;
+    private TaskRepository repository;
+    private int currentPage = 1;
+    private boolean isLoading = false;
+    private static final int PRE_LOAD_ITEMS = 1;
+    private FirebaseAuth mAuth;
     private TaskAdapter taskAdapter;
     public TasksFragment() {
         // Required empty public constructor
@@ -30,9 +43,27 @@ public class TasksFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentTasksBinding.inflate(inflater, container, false);
 
-        binding.rcvTasks.setLayoutManager(new LinearLayoutManager(getContext()));
-        taskAdapter = new TaskAdapter(TaskData.SAMPLE_TASKS);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        binding.rcvTasks.setLayoutManager(layoutManager);
+        taskAdapter = new TaskAdapter();
         binding.rcvTasks.setAdapter(taskAdapter);
+        repository = new TaskRepository();
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.rcvTasks.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int totalItemCount = layoutManager.getItemCount();
+                int lastVisibleItem = layoutManager.findLastVisibleItemPosition();
+
+                if (!isLoading && totalItemCount <= (lastVisibleItem + PRE_LOAD_ITEMS)) {
+                    loadTasks();
+                }
+            }
+        });
+
         return binding.getRoot();
     }
 
@@ -40,5 +71,45 @@ public class TasksFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadTasks();
+    }
+
+    private void loadTasks()  {
+      //  isLoading = true;
+        showProgressBar();
+        String currentUserId = "1249588e-aea4-4a9e-930d-0778c8669364";
+       // String currentUserId = mAuth.getCurrentUser().getUid();
+        repository.getTasks(currentPage, currentUserId, new IApiCallback<List<Task>>() {
+            @Override
+            public void onSuccess(List<Task> tasks) {
+                if(!tasks.isEmpty()){
+                    taskAdapter.addTasks(tasks);
+                    currentPage++;
+                }
+                isLoading = false;
+                hideProgressBar();
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
+                isLoading = false;
+                hideProgressBar();
+            }
+
+        });
+    }
+
+    private void showProgressBar() {
+        binding.tasksProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        binding.tasksProgressBar.setVisibility(View.GONE);
     }
 }
