68ddcd3887ccdb9a3d5597b5807c3bf36e17e202
sopheak <hangsopheak.hod@gmail.com>
2025-03-18 18:39:45 +0700

    week 6 - firebase auth

--- app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.taskmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +17,36 @@ import com.example.taskmanagement.fragment.CalendarFragment;
 import com.example.taskmanagement.fragment.CategoriesFragment;
 import com.example.taskmanagement.fragment.SettingFragment;
 import com.example.taskmanagement.fragment.TasksFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.signout) {
+            mAuth.signOut();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        } else if (itemId == R.id.newtask) {
+            Intent intent = new Intent(this, NewTaskActivity.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // For explicit Toolbar from layout
+        setSupportActionBar(binding.topAppBar);
+        getSupportActionBar().setTitle("");
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         binding.bottomNavigation.setOnItemSelectedListener(item -> {
            int itemId = item.getItemId();
@@ -46,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         binding.bottomNavigation.setSelectedItemId(R.id.nav_tasks);
-
     }
 
     private void LoadFragment(Fragment fragment) {
